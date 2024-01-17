8ed22988c0f29710afdd1543aca01c133e689dd0
FERNANDO CALIXTO <107202032+fernandocalixto30@users.noreply.github.com>
2024-01-17 12:07:41 -0300

    Add files via upload

---
+++ tabela/script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    var evento1 = document.getElementById('evento1');
+    var evento = document.getElementById('evento');
+    evento.addEventListener('click', function () {
+    
+        colorButton.style.backgroundColor = randomColor;
+});
+});
